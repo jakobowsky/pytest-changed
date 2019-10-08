6b7a912a6fff6f4386ea68e4b69c6604af6a2cec
Alex Mühlbauer <a.muehlbauer@gastrofix.com>
2019-10-08 15:27:22 +0200

    applying enforced styling rules of flake8

--- tests/test_changed.py
+++ tests/test_changed.py
@@ -7,7 +7,8 @@ except ImportError:
 
 GIT_DIFF_CHANGE_IN_FUNCTION = \
     b'diff --git a/tests/dummy_test.py b/tests/dummy_test.py\n' \
-    b'index 6575e440acc807efabcbc156b8683c6344b6fda4..11314b99949c7e987f050255fa4d0c6497c529bc 100644\n' \
+    b'index 6575e440acc807efabcbc156b8683c6344b6fda4..' \
+    b'11314b99949c7e987f050255fa4d0c6497c529bc 100644\n' \
     b'--- a/tests/dummy_test.py\n' \
     b'+++ b/tests/dummy_test.py\n' \
     b'@@ -4,6 +4,7 @@ import pytest\n' \
@@ -32,7 +33,8 @@ GIT_DIFF_CHANGE_IN_FUNCTION = \
 
 GIT_DIFF_CHANGE_IN_CLASS = \
     b'diff --git a/tests/dummy_test.py b/tests/dummy_test.py\n' \
-    b'index 6575e440acc807efabcbc156b8683c6344b6fda4..11314b99949c7e987f050255fa4d0c6497c529bc 100644\n' \
+    b'index 6575e440acc807efabcbc156b8683c6344b6fda4..' \
+    b'11314b99949c7e987f050255fa4d0c6497c529bc 100644\n' \
     b'--- a/tests/dummy_test.py\n' \
     b'+++ b/tests/dummy_test.py\n' \
     b'@@ -4,6 +4,7 @@ import pytest\n' \
@@ -80,7 +82,8 @@ def test_output_no_changes(get_changed_files_mock, testdir, config_mock):
 @patch("pytest_changed.get_changed_files")
 def test_output_changed_function(get_changed_files_mock, testdir, config_mock):
     """
-    In case of changes just to specific functions only their names will be detected.
+    In case of changes just to specific functions only their names
+    will be detected.
     """
     diff = MagicMock()
     diff.diff = GIT_DIFF_CHANGE_IN_FUNCTION
@@ -99,18 +102,25 @@ def test_output_changed_function(get_changed_files_mock, testdir, config_mock):
         assert "Changed test files... 1. {" \
                "'%s/dummy_test.py': [" \
                "'test_class_one_test_one', " \
-               "'test_class_two_test_two']}" % str(testdir.tmpdir) in result.stdout.str()
+               "'test_class_two_test_two']}" % str(testdir.tmpdir) in \
+               result.stdout.str()
 
 
 @patch("pytest_changed.get_changed_files")
-def test_output_changed_class_and_function(get_changed_files_mock, testdir, config_mock):
+def test_output_changed_class_and_function(
+        get_changed_files_mock,
+        testdir,
+        config_mock
+):
     """
-    In case of changes inside the class body, the class names should be detected.
+    In case of changes inside the class body, the class names should
+    be detected.
 
-    To make sure all tests inside the class are running as changes in the class definition
-    may affect test functions defined in the scope of the class.
+    To make sure all tests inside the class are running as changes in the class
+    definition may affect test functions defined in the scope of the class.
 
-    When pytest receives a class it will run all test-functions that are contained.
+    When pytest receives a class it will run all test-functions
+    that are contained.
     """
     diff = MagicMock()
     diff.diff = GIT_DIFF_CHANGE_IN_CLASS
