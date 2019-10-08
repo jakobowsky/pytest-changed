e5fdcf15560e78976f7878457b3ad19d241ab32b
Alex Mühlbauer <a.muehlbauer@gastrofix.com>
2019-10-08 09:35:35 +0200

    updating dummy test diff

--- tests/test_changed.py
+++ tests/test_changed.py
@@ -3,43 +3,30 @@ from unittest.mock import patch, MagicMock
 
 from pytest_changed import get_changed_files_with_functions
 
-GIT_DIFF_WITH_CHANGES = b'diff --git a/tests/app/test_basic_functions/log_in_log_out/log_in_feature_test.py b/tests/app/test_basic_functions/log_in_log_out/log_in_feature_test.py\n' \
+GIT_DIFF_WITH_CHANGES = b'diff --git a/tests/dummy_test.py b/tests/dummy_test.py\n' \
                         b'index 6575e440acc807efabcbc156b8683c6344b6fda4..11314b99949c7e987f050255fa4d0c6497c529bc 100644\n' \
-                        b'--- a/tests/app/test_basic_functions/log_in_log_out/log_in_feature_test.py\n' \
-                        b'+++ b/tests/app/test_basic_functions/log_in_log_out/log_in_feature_test.py\n' \
+                        b'--- a/tests/dummy_test.py\n' \
+                        b'+++ b/tests/dummy_test.py\n' \
                         b'@@ -4,6 +4,7 @@ import pytest\n' \
-                        b' class TestLogInFeature:\n' \
-                        b'     @pytest.fixture(scope=\'function\', autouse=True)\n' \
-                        b'     def _setup_teardown(self, master):\n' \
+                        b' class TestClassOne:\n' \
                         b'+\n' \
-                        b'         master.main_screen.logout()  # setup\n' \
-                        b'         yield  # test execution\n' \
-                        b'         # teardown\n'
+                        b'     @pytest.fixture(autouse=True)\n' \
+                        b'     def fixture_one(self, db):\n' \
+                        b'         db.init()\n' \
+                        b'         yield\n' \
+                        b'         db.quit()\n'
 
-GIT_DIFF_WITH_CHANGES_2 = b'diff --git a/tests/app/test_basic_functions/log_in_log_out/log_out_feature_test.py b/tests/app/test_basic_functions/log_in_log_out/log_out_feature_test.py\n' \
-                          b'index 8e833cd726424aa0961fa62d54b53a8f37aee227..78f3f3f68074b8f072b54622d7d7aec22557f0fe 100644\n' \
-                          b'--- a/tests/app/test_basic_functions/log_in_log_out/log_out_feature_test.py\n' \
-                          b'+++ b/tests/app/test_basic_functions/log_in_log_out/log_out_feature_test.py\n' \
-                          b'@@ -4,6 +4,7 @@ from framework.qa_constants.qa_waiters.qa_waiters import QAWaiters\n' \
-                          b' \n' \
-                          b' \n' \
-                          b' class TestLogoutFeature:\n' \
-                          b'+\n' \
-                          b'     def test_logout_via_employee_menu(self, master):\n' \
-                          b'         master.main_screen.employee_button.tap()\n' \
-                          b'         master.main_screen.employee_choice_popover.logout_button.tap()\n'
-
-GIT_DIFF_NO_CHANGES = b'diff --git a/tests/app/test_basic_functions/log_in_log_out/log_in_feature_test.py b/tests/app/test_basic_functions/log_in_log_out/log_in_feature_test.py\n' \
+GIT_DIFF_NO_CHANGES = b'diff --git a/tests/dummy_test.py b/tests/dummy_test.py\n' \
                       b'index 6575e440acc807efabcbc156b8683c6344b6fda4..11314b99949c7e987f050255fa4d0c6497c529bc 100644\n' \
-                      b'--- a/tests/app/test_basic_functions/log_in_log_out/log_in_feature_test.py\n' \
-                      b'+++ b/tests/app/test_basic_functions/log_in_log_out/log_in_feature_test.py\n' \
+                      b'--- a/tests/dummy_test.py\n' \
+                      b'+++ b/tests/dummy_test.py\n' \
                       b'@@ -4,6 +4,7 @@ import pytest\n' \
-                      b' class TestLogInFeature:\n' \
-                      b'     @pytest.fixture(scope=\'function\', autouse=True)\n' \
-                      b'     def _setup_teardown(self, master):\n' \
-                      b'         master.main_screen.logout()  # setup\n' \
-                      b'         yield  # test execution\n' \
-                      b'         # teardown\n'
+                      b' class TestClassOne:\n' \
+                      b'     @pytest.fixture(autouse=True)\n' \
+                      b'     def fixture_one(self, db):\n' \
+                      b'         db.init()\n' \
+                      b'         yield\n' \
+                      b'         db.quit()\n'
 
 
 def test_shows_changed_tests(testdir):
@@ -69,7 +56,7 @@ def test_get_changed_files(get_changed_files_mock, testdir):
     get_changed_files_mock.return_value = (modified_mock, added_mock)
     with patch("pytest_changed.Repo"):
         changed = get_changed_files_with_functions(config=config_mock)
-        assert changed == {f'{testdir.tmpdir}/tests/dummy_test.py': ['TestLogoutFeature']}
+        assert changed == {f'{testdir.tmpdir}/tests/dummy_test.py': ['TestClassOne']}
 
 
 @patch("pytest_changed.get_changed_files")
@@ -97,15 +84,12 @@ def test_get_changed_files_output(get_changed_files_mock, testdir):
     get_changed_files_mock.return_value = (modified_mock, added_mock)
     with patch("pytest_changed.Repo"):
         result = testdir.runpytest("--changed")
-        assert "Changed test files... 1. {{'{}/dummy_test.py': ['TestLogoutFeature']}}".format(
+        assert "Changed test files... 1. {{'{}/dummy_test.py': ['TestClassOne']}}".format(
             testdir.tmpdir) in result.stdout.str()
 
 
 @patch("pytest_changed.get_changed_files")
 def test_get_changed_files_output_no_changes(get_changed_files_mock, testdir):
-    diff = MagicMock()
-    diff.diff = GIT_DIFF_NO_CHANGES
-    diff.a_path = "dummy_test.py"
 
     testdir.makepyfile(".py", dummy_test="""
         def dummy_test():
@@ -113,7 +97,7 @@ def test_get_changed_files_output_no_changes(get_changed_files_mock, testdir):
     """)
 
     modified_mock = MagicMock()
-    modified_mock.__iter__ = MagicMock(return_value=iter([diff]))
+    modified_mock.__iter__ = MagicMock(return_value=iter([]))
 
     added_mock = MagicMock()
     added_mock.__iter__ = MagicMock(return_value=iter([]))
