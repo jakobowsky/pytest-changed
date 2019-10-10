b6b4e8edd4a9c5409279cfe620e6e4706749b14d
Alex Mühlbauer <a.muehlbauer@gastrofix.com>
2019-10-10 10:05:08 +0200

    adds test checking if the plugin acts correctly for code just being removed.

--- tests/test_changed.py
+++ tests/test_changed.py
@@ -57,6 +57,62 @@ GIT_DIFF_CHANGE_IN_CLASS = \
     b'     def test_class_two_test_two(self):\n' \
     b'         assert 1 + 1 == 2\n'
 
+GIT_DIFF_CODE_REMOVED = \
+    b'diff --git a/tests/dummy_test.py b/tests/dummy_test.py\n' \
+    b'index 6575e440acc807efabcbc156b8683c6344b6fda4..' \
+    b'11314b99949c7e987f050255fa4d0c6497c529bc 100644\n' \
+    b'--- a/tests/dummy_test.py\n' \
+    b'+++ b/tests/dummy_test.py\n' \
+    b'@@ -4,6 +4,7 @@ import pytest\n' \
+    b' class TestClassOne:\n' \
+    b'\n' \
+    b'-\n' \
+    b'-\n' \
+    b'     def test_class_one_test_one(self):\n' \
+    b'         assert 1 + 1 == 2\n' \
+    b'\n' \
+    b'     def test_class_one_test_two(self):\n' \
+    b'         assert 1 + 1 == 2\n' \
+    b'\n' \
+    b'\n' \
+    b' class TestClassTwo:\n' \
+    b'\n' \
+    b'-\n' \
+    b'-\n' \
+    b'     def test_class_two_test_one(self):\n' \
+    b'         assert 1 + 1 == 2\n' \
+    b'\n' \
+    b'     def test_class_two_test_two(self):\n' \
+    b'         assert 1 + 1 == 2\n'
+
+GIT_DIFF_CODE_REMOVED_IN_FUNCTION = \
+    b'diff --git a/tests/dummy_test.py b/tests/dummy_test.py\n' \
+    b'index 6575e440acc807efabcbc156b8683c6344b6fda4..' \
+    b'11314b99949c7e987f050255fa4d0c6497c529bc 100644\n' \
+    b'--- a/tests/dummy_test.py\n' \
+    b'+++ b/tests/dummy_test.py\n' \
+    b'@@ -4,6 +4,7 @@ import pytest\n' \
+    b' class TestClassOne:\n' \
+    b'\n' \
+    b'     def test_class_one_test_one(self):\n' \
+    b'-\n' \
+    b'-\n' \
+    b'         assert 1 + 1 == 2\n' \
+    b'\n' \
+    b'     def test_class_one_test_two(self):\n' \
+    b'         assert 1 + 1 == 2\n' \
+    b'\n' \
+    b'\n' \
+    b' class TestClassTwo:\n' \
+    b'\n' \
+    b'     def test_class_two_test_one(self):\n' \
+    b'-\n' \
+    b'-\n' \
+    b'         assert 1 + 1 == 2\n' \
+    b'\n' \
+    b'     def test_class_two_test_two(self):\n' \
+    b'         assert 1 + 1 == 2\n'
+
 
 def test_shows_changed_tests(testdir):
     with patch("pytest_changed.Repo"):
@@ -140,3 +196,61 @@ def test_output_changed_class_and_function(
                "'%s/dummy_test.py': [" \
                "'TestClassOne', " \
                "'TestClassTwo']}" % str(testdir.tmpdir) in result.stdout.str()
+
+
+@patch("pytest_changed.get_changed_files")
+def test_output_removed_code(
+        get_changed_files_mock,
+        testdir,
+        config_mock
+):
+    """
+    In case of code was just removed it should still act the same as for any
+    other change be detected. If the removal happened on class level the whole
+    class must be detected as changed.
+    """
+    diff = MagicMock()
+    diff.diff = GIT_DIFF_CODE_REMOVED
+    diff.a_path = "dummy_test.py"
+
+    modified_mock = MagicMock()
+    modified_mock.__iter__ = MagicMock(return_value=iter([diff]))
+
+    added_mock = MagicMock()
+    added_mock.__iter__ = MagicMock(return_value=iter([]))
+
+    get_changed_files_mock.return_value = (modified_mock, added_mock)
+
+    with patch("pytest_changed.Repo"):
+        result = testdir.runpytest("--changed")
+        assert "Changed test files... 1. {" \
+               "'%s/dummy_test.py': [" \
+               "'TestClassOne', " \
+               "'TestClassTwo']}" % str(testdir.tmpdir) in result.stdout.str()
+
+
+@patch("pytest_changed.get_changed_files")
+def test_output_removed_function(get_changed_files_mock, testdir, config_mock):
+    """
+    In case of code removal just to specific functions
+    their names will also be detected.
+    """
+    diff = MagicMock()
+    diff.diff = GIT_DIFF_CODE_REMOVED_IN_FUNCTION
+    diff.a_path = "dummy_test.py"
+
+    modified_mock = MagicMock()
+    modified_mock.__iter__ = MagicMock(return_value=iter([diff]))
+
+    added_mock = MagicMock()
+    added_mock.__iter__ = MagicMock(return_value=iter([]))
+
+    get_changed_files_mock.return_value = (modified_mock, added_mock)
+
+    with patch("pytest_changed.Repo"):
+        result = testdir.runpytest("--changed")
+        assert "Changed test files... 1. {" \
+               "'%s/dummy_test.py': [" \
+               "'test_class_one_test_one', " \
+               "'test_class_two_test_one']}" % str(testdir.tmpdir) in \
+               result.stdout.str()
